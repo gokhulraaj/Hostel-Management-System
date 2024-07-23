2281c24666a92a9f845641acca20e9c1a8b68384
gokhulraaj <gokhulraaj@gmail.com>
2024-07-23 09:47:41 +0530

    Initial commit with project files

---
+++ frontend/src/components/footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Container, Row, Col } from 'react-bootstrap'
+
+const Footer = () => {
+  return (
+    <Container>
+      <Row>
+        {/* <Col className='text-center py-3'>&copy;BY: KEC HOSTEL</Col> */}
+      </Row>
+    </Container>
+  )
+}
+
+export default Footer
